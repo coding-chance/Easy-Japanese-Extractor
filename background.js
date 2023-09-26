421e5fc3a7cbacdf89de0694bf338f89d124eebe
coding-chance <90079117+coding-chance@users.noreply.github.com>
2023-09-26 09:18:18 +0200

    Added shortcut key

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
 /** Activate shortcut key (send signal to content.js when shortcut key is pressed) */
-// chrome.commands.onCommand.addListener((command) => {
-//   if (command === 'send-continue-message') {
-//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//       chrome.tabs.sendMessage(tabs[0].id, { command: 'send-message' }, (response) => {
-//         console.log(response.message);
-//       });
-//     });
-//   }
-// });
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Received command: ${command}`);
+
+    // Extract Dialogues
+    if (command === '_extract_dialogues') {
+        // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        //     chrome.tabs.sendMessage(tabs[0].id, { command: 'send-message' }, (response) => {
+        //         console.log(response.message);
+        //     });
+        // });
+    }
+
+    // Extract Vocabulary
+    if (command === '_extract_vocabulary') {
+        // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        //     chrome.tabs.sendMessage(tabs[0].id, { command: 'send-message' }, (response) => {
+        //         console.log(response.message);
+        //     });
+        // });
+    }
+});
