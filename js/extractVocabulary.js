ce199a6a83e994d2ed4f2c9c61412b6487cb8571
coding-chance <90079117+coding-chance@users.noreply.github.com>
2023-09-26 10:45:59 +0200

    Added download txt and csv

--- js/extractVocabulary.js
+++ js/extractVocabulary.js
@@ -11,6 +11,7 @@ function extractVocabulary() {
     let vocabularySection = document.getElementById("vocabulary");
     let textBoxes = vocabularySection.querySelectorAll(".text-box");
     var vocabularyList = [];
+    var vocabularyCsvOutputList = [];
     textBoxes.forEach((element, index)=>{
         let dialoguePElements = element.getElementsByTagName("p");
         let strings = [];
@@ -19,8 +20,12 @@ function extractVocabulary() {
             strings.push(text);
         }
         vocabularyList.push(`${strings[0]} [ ${strings[1]} ]    ${strings[2]}`);
+        vocabularyCsvOutputList.push(`${strings[0]},${strings[1]},${strings[2]}`);
     })
     let vocabularyString = vocabularyList.join("\n");
+    let vocabularyCsvString = vocabularyCsvOutputList.join("\n");
+    console.log(vocabularyString);
+
     const copyToClip = async (vocabularyString) => {
       try {
         await navigator.clipboard.writeText(vocabularyString);
@@ -30,8 +35,28 @@ function extractVocabulary() {
         alert('Failed to copy vocabulary to clipboard. Check console on your browser.');
       }
     }
-    
     alert("Turn off the extension pop-up. \n(Click anywhere on the page body to disable the pop-up)")
-    copyToClip(vocabularyString);
+    setTimeout(() => { copyToClip(vocabularyString) }, 500);
     
+    // Save as txt
+    let blob = new Blob([vocabularyString], { type: 'text/plain' });
+    let aTag = document.createElement('a');
+    aTag.href = URL.createObjectURL(blob);
+    aTag.target = '_blank';
+    let lessonNumber = document.querySelector(".cando-block .num").innerText;
+    let fileName = `${lessonNumber}-vocabulary_nhk-jp-conversation.txt`
+    aTag.download = fileName;
+    aTag.click();
+    URL.revokeObjectURL(aTag.href);
+
+    // Save as csv
+    const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+    blob = new Blob([bom, vocabularyCsvString], { type: 'text/csv' });
+    aTag = document.createElement('a');
+    aTag.href = URL.createObjectURL(blob);
+    aTag.target = '_blank';
+    fileName = `${lessonNumber}-vocabulary_nhk-jp-conversation.csv`
+    aTag.download = fileName;
+    aTag.click();
+    URL.revokeObjectURL(aTag.href);
   }
